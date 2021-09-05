f0cbb5d1c2e0c07fd7e74a836652d302c7d26737
after909 <80917206+after909@users.noreply.github.com>
2021-09-05 00:47:10 -0600

    test2

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const switchElement = document.querySelector('.switch');
+const footerElement = document.querySelector('footer');
 switchElement.addEventListener('click', () => {
     document.body.classList.toggle('dark');
+    footerElement.classList.toggle('dark');
     switchElement.classList.toggle('switch-light');
 })
